bfeaefbfb80c93903226632788fdd6b6e52c0ee5
Senad Shabaj <senadshabaj73@gmail.com>
2022-06-22 00:39:40 +0200

    AddRecordPage

--- classes/utilities/static_variables.py
+++ classes/utilities/static_variables.py
@@ -12,12 +12,14 @@ class StaticVariables():
     PASSWRODS_DONT_MATCH = "Passwords don't match"
     OLD_PASSWORD_INCORRECT = 'Old password is incorrect'
     ACCOUNT_VERIFIED = "Account Verified"
+    ADD_RECORD = "Add Record"
     
     # PAGES
     LOGIN_PAGE = 'LoginPage'
     LANDING_PAGE = 'LandingPage'
     CHANGE_PASSWORD_PAGE = 'ChangePasswordPage'
     CHANGE_EMAIL_PAGE = 'ChangeEmailPage'
+    RECORD_LIST_PAGE = 'RecordListPage'
 
     ACCOUNT = "Account"
     
@@ -28,9 +30,16 @@ class StaticVariables():
     SMALL_COBRA_LOGO_PATH = './static/small_cobra_logo.png'
     COBRA_LOGO_PATH = r'./static/cobra_logo.ico'
     LOGIN_LOGO_PATH = './static/login_logo.png'
+    USERNAME_LOGO_PATH = f'./static/user_logo.png'
+    WEBSITE_LOGO_PATH = './static/website_logo.png'
+    NEXT_LOGO_PATH = './static/next_logo.png'
+    SAVE_LOGO_PATH = './static/save_logo.png'
+    LENGTH_LOGO_PATH = './static/length_logo.png'
+    GENERATE_PASSWORD_LOGO_PATH = './static/generate_password_logo.png'
     
     
     # PAGE TEXT
     ACC_VERIFIED = 'Account verified. Please login to begin.'
+    GENERATE_PASSWORD = 'Generate Password'
     
 
