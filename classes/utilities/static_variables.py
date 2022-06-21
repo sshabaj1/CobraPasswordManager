3da27d3a5851acd88bb560e876a9c795e42ff808
Senad Shabaj <senadshabaj73@gmail.com>
2022-06-22 00:28:12 +0200

    AccountVerifiedPage

--- classes/utilities/static_variables.py
+++ classes/utilities/static_variables.py
@@ -11,6 +11,7 @@ class StaticVariables():
     PASSWORD_CHANGED = 'Password Changed'
     PASSWRODS_DONT_MATCH = "Passwords don't match"
     OLD_PASSWORD_INCORRECT = 'Old password is incorrect'
+    ACCOUNT_VERIFIED = "Account Verified"
     
     # PAGES
     LOGIN_PAGE = 'LoginPage'
@@ -24,6 +25,12 @@ class StaticVariables():
     BACK_LOGO_PATH = './static/back_logo.png'
     EMAIL_LOGO_PATH = './static/email_logo.png'
     PASSWORD_LOGO_PATH = './static/password_logo.png'
-    SMALL_COBRA_LOGO = './static/small_cobra_logo.png'
+    SMALL_COBRA_LOGO_PATH = './static/small_cobra_logo.png'
+    COBRA_LOGO_PATH = r'./static/cobra_logo.ico'
+    LOGIN_LOGO_PATH = './static/login_logo.png'
+    
+    
+    # PAGE TEXT
+    ACC_VERIFIED = 'Account verified. Please login to begin.'
     
 
